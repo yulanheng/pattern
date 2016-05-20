749971bb008ebda002a609be1fd73ed51e0df71a
yulanheng <yulanheng@news.cn>
2016-05-20 10:56:03 +0800

    update  filter

--- src/main/java/cn/ylh/pattern/chain/Filter.java
+++ src/main/java/cn/ylh/pattern/chain/Filter.java
@@ -1,6 +1,11 @@
 package cn.ylh.pattern.chain;
 
 public interface Filter {
-
+	/**
+	 *
+	 * @param request
+	 * @param response
+	 * @param chain
+     */
 	void doFilter(Request request,Response response,FilterChain chain);
 }
