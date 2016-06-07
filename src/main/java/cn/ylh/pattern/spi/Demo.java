43b7c8122db66a27e1a811863ed5a4c8df2ee044
yulanheng <yulanheng@qiyi.com>
2016-06-07 18:03:29 +0800

    123

--- src/main/java/cn/ylh/pattern/spi/Demo.java
+++ src/main/java/cn/ylh/pattern/spi/Demo.java
@@ -4,6 +4,6 @@ public class Demo {
 
 	public static void main(String[] args) {
 		MessageService service=MessageServiceManager.getMessageService("cn.ylh.pattern.spi.raw.RawMessageService");
-		System.out.println(service.getMessage());
+		System.out.println(service.getMessage());    
 	}
 }
